ec05e584b606be5992ac00034411540c3411f918
odevoo <elandry@odevoo.com>
2016-11-15 16:48:02 +0100

    commit

--- log.php
+++ log.php
@@ -65,15 +65,15 @@ switch ($_GET{'mode'}) {
 
             ?>
             <tr>
-                <td><?=$task['name']?></td>
-                <td><?=date_nice($task['date_start'])?></td>
-                <td><?
+                <td><?php echo $task['name']?></td>
+                <td><?php echo date_nice($task['date_start'])?></td>
+                <td><?php 
                 if ($task['date_end'] != "") {
                     echo date_nice($task['date_end']);
                 } 
 
                     ?></td>
-                <td><?
+                <td><?php
                 if ($task['date_end'] == "") {
                     echo time_nice(time() -$task['date_start']);
                 } else {
